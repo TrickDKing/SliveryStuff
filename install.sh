3f4b97d1a01ef98ca657f620f6b199fbc97a9579
Xavier Lim <xav2601@gmail.com>
2024-09-30 03:09:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+
+if [[ "$EUID" -ne 0 ]];then
+    echo "Please run as root"
+    exit
+fi
+
 sudo apt update
-sudo apt install mingw-w64 binutils-mingw-w64 g++-mingw64
+sudo apt install mingw-w64 binutils-mingw-w64 g++-mingw-w64
 VER=$(curl --silent -qI https://github.com/BishopFox/sliver/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}');\ 
 wget https://github.com/BishopFox/sliver/releases/tag/$VER/sliver-server_linux
 wget https://github.com/BishopFox/sliver/releases/tag/$VER/sliver-client_linux
