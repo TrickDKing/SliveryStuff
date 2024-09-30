a1876a44c5ab32b4c7ded03a1f4596f5ba8757b1
Xavier Lim <xav2601@gmail.com>
2024-09-30 03:14:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 sudo apt update
 sudo apt install mingw-w64 binutils-mingw-w64 g++-mingw-w64
 VER=$(curl --silent -qI https://github.com/BishopFox/sliver/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}');
-wget https://github.com/BishopFox/sliver/releases/tag/$VER/sliver-server_linux
-wget https://github.com/BishopFox/sliver/releases/tag/$VER/sliver-client_linux
+wget https://github.com/BishopFox/sliver/releases/download/$VER/sliver-server_linux
+wget https://github.com/BishopFox/sliver/releases/download/$VER/sliver-client_linux
 chmod +x sliver-server_linux
 chmod +x sliver-client_linux
